9b77e4dd29b52f4edbea6287017b690082b00715
FranklinApaza <125500708+FranklinApaza@users.noreply.github.com>
2023-06-02 22:36:12 -0400

    Curso NodeJS

--- appnode/routes/index.js
+++ appnode/routes/index.js
@@ -21,7 +21,7 @@ module.exports = (app) => {
     app.post('/api/pruebapost', mainController.pruebaPost);
 
     app.post('/api/productos', productoController.create);
-    app.get('/api/productos', productoController.list);
+    app.get('/api/productos', auth.verificaUsuario, productoController.list);
     app.get('/api/productos/:id', productoController.getProductById);
     app.put('/api/productos/:id', productoController.update);
     app.delete('/api/productos/:id', productoController.delete);
@@ -30,6 +30,20 @@ module.exports = (app) => {
 
     app.get('/api/cuentas', mainController.listCuentas);
 
+    /* Para probar la venta - venta_detalle, primero actualizar la tabla venta_detalle incorporando cantidad y enviando los siguientes datos en el body
+    {
+        "total": 1234,
+        "productos": [
+        {"id":3, "cantidad": 2,"precio": 222},
+        {"id":4, "cantidad": 1,"precio": 888},
+        {"id":5, "cantidad": 5,"precio": 22}
+        ],
+        "cliente": {
+        "nombre":"Cliente Nuevo",
+        "telefono":"123456",
+        "email":"correo@email.com"}
+    }
+    */
     app.post('/api/ventas', ventaController.create);
 
     app.post('/api/usuarios', usuarioController.create);
